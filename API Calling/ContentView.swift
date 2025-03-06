f76c590ebac36875e23ad6b698e71ec09c6e787a
Samantha Hiles <samih11@att.net>
2025-03-06 09:34:05 -0600

    Initial Commit

---
+++ API Calling/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  API Calling
+//
+//  Created by Samantha Hiles on 3/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
